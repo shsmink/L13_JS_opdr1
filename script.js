87888b7f6d9374522def3f6fb033e76e03fba59a
shsmink <79309340+shsmink@users.noreply.github.com>
2021-03-26 18:52:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var age = 18;
+if (age > 17){
+    console.log("Goedenavond welkom, kom erin!");
+} else if (age < 18){
+    console.log("Goedenavond! Ik kan je helaas niet binnenlaten, je bent te jong.");
+}
+
+var isFemale = true;
+if (isFemale){
+    console.log("Bedankt dame, loop maar door, fijne avond!");
+} else {
+    console.log("Helaas jongeman ik kan je niet binnenlaten, het is vanavond ladiesnight.");
+} 
+
+var driverStatus = "bob";
+if (driverStatus == "bob"){
+    console.log("Geen slokje alcohol, super! Zorg jij dat je vriendinnen veilig thuiskomen?");
+} else {
+    console.log("Je hebt teveel gedronken, je mag zo niet naar huis rijden. Zal ik een taxi voor je bellen?");
+} 
